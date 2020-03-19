8dec8a6fa4b087e3699ec205f7ac640fdcfabfab
Nirmal Kumar <codingboredom@gmail.com>
2020-03-19 23:14:13 +0530

    First Commit

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import Header from "../common/Header";
+import image from "../assets/img/header-bg.jpg"
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header
+                    title="Welcome to out studio!"
+                    subtitle="IT'S NICE TO MEET YOU"
+                    buttonText="Tell me more"
+                    link="/services"
+                    showButton={true}
+                    image={image}
+                />
+            </div>
+        );
+    }
+}
+
+export default Home;
