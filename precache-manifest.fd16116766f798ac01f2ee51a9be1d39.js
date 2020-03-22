036dfb68cea96ce00ee1c029af2385055af1be17
Nirmal Kumar <codingboredom@gmail.com>
2020-03-22 14:31:09 +0530

    Updates

---
+++ precache-manifest.fd16116766f798ac01f2ee51a9be1d39.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b773234ae41a849a37974d1b102579",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "d998692b0b1990423bae",
+    "url": "/react-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "df9a087c84820fa9ae68",
+    "url": "/react-website/static/js/2.a185b347.chunk.js"
+  },
+  {
+    "revision": "e5b35a3593b1c83c3e03fdfdef365a48",
+    "url": "/react-website/static/js/2.a185b347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d998692b0b1990423bae",
+    "url": "/react-website/static/js/main.fa4be793.chunk.js"
+  },
+  {
+    "revision": "2354a1a30480776255b2",
+    "url": "/react-website/static/js/runtime-main.b8173d8a.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/react-website/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/react-website/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/react-website/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/react-website/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/react-website/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/react-website/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/react-website/static/media/about.a90b0f86.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-website/static/media/header-bg.e2d2a51c.jpg"
+  }
+]);
